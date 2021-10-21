6b7d2872e4794186cfe5c54ea8680c0d89d78fad
sspinner224 <sspinner@quinnipiac.edu>
2021-10-21 19:02:40 -0400

    moon sign code unfinished

--- src/App.java
+++ src/App.java
@@ -7,13 +7,40 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.util.Scanner;
+import java.sql.Time;
 
-
-public class App extends JFrame {
+public class App {
 
     private BufferedImage starrySky;
 
     public App(){
+        System.out.println("Welcome to our astrology calculator app.\n" +
+                "If you would like to find out information about your birth chart, please provide the following" +
+                "information in number format.\n(Ex. Jan = 1)\n" +
+                "What is your birth month?");
+
+        Scanner scanner = new Scanner(System.in);
+        int birthMonth = scanner.nextInt();
+
+        System.out.println("What is your birth day?");
+        int birthDay = scanner.nextInt();
+
+        System.out.println("What is your birth year?");
+        int birthYear = scanner.nextInt();
+
+//        System.out.println("What is your birth time?");
+//        Time birthTime = Time.valueOf(scanner.next());
+
+        AstrologyInfo astrologyInfo = new AstrologyInfo(birthMonth, birthDay, birthYear);
+        astrologyInfo.getSunSign();
+
+        System.out.println("Please enter your birth hour as a number. \n" +
+                "For example: if you were born at 7pm, please enter 19.");
+        int birthTime = scanner.nextInt();
+        astrologyInfo.getMoonSign();
+
+        /*
         super("Birth Chart App");
         this.setSize(500, 400);
         this.setVisible(true);
@@ -28,6 +55,10 @@ public class App extends JFrame {
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.validate();
+        */
+
+
+
 
     }
     public static void main(String[] args) {
