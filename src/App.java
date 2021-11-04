d7f001fadd8c935e8449bbef2ad154250e8be8e5
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-11-04 16:46:50 -0400

    javafx

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.awt.BorderLayout;
@@ -23,8 +25,14 @@ public class App extends Application{
 	
 	 
 	 @Override
-		public void start(Stage arg0) throws Exception {
+		public void start(Stage primaryStage) throws Exception {
 			// TODO Auto-generated method stub
+		 
+		 BorderPane root = new BorderPane();
+		 Scene scene = new Scene(root,400,400);
+		 scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		 primaryStage.setScene(scene);
+		 primaryStage.show();
 			
 		}
     
@@ -84,7 +92,7 @@ public class App extends Application{
 
     }
     public static void main(String[] args) {
-        new App();
+       launch(args);
     }
 	
 
